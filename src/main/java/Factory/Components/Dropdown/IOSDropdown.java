e6ab52004d0f39d4de66b4eb435b9a48339826ed
geetanjali3333 <geetanjaligadiraju123@gmail.com>
2025-02-09 08:03:34 +0530

    Factory method, simple factory, abstract factory

---
+++ src/main/java/Factory/Components/Dropdown/IOSDropdown.java
@@ -0,0 +1,8 @@
+package Factory.Components.Dropdown;
+
+public class IOSDropdown extends Dropdown{
+    @Override
+    public void options() {
+        System.out.println("Options from IOS Dropdown");
+    }
+}
